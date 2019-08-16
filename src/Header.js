dcedcd9f036c19a4f87b5a1da34340741a820f8b
ReidCampbell <reidscottcampbell@gmail.com>
2019-08-16 12:10:49 +0100

    testing on production

--- src/Header.js
+++ src/Header.js
@@ -3,22 +3,24 @@ import React, { Component } from "react";
 import "./Header.css";
 
 class Header extends Component {
-    constructor(props) {
-        super(props);
+    // constructor(props) {
+    //     super(props);
 
-        this.state = {
-            videoURL:
-                "https://raw.githubusercontent.com/youtube/api-samples/master/java/src/main/resources/sample-video.mp4"
-        };
-    }
+    //     this.state = {
+    //         videoURL:
+    //             "https://raw.githubusercontent.com/youtube/api-samples/master/java/src/main/resources/sample-video.mp4"
+    //     };
+    // }
 
     render() {
         return (
             <>
                 <div className="video-container">
-                    <video id="background-video" loop autoPlay>
-                        <source src={this.state.videoURL} type="video/mp4" />
-                        <source src={this.state.videoURL} type="video/ogg" />
+                    <video id="background-video" loop autoPlay muted>
+                        <source
+                            src={require("./images/header.mp4")}
+                            type="video/mp4"
+                        />
                         Your browser does not support the video tag.
                     </video>
                     <div className="logo-img">
